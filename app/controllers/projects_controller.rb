a07b477de62bc34ef34334370ac6ce41ee458742
motorkey <auagcumitsumo@gmail.com>
2015-05-23 23:55:32 +0900

    Update design of projects/index.html.slim

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,5 @@
 class ProjectsController < ApplicationController
 
-  # app/views/layout/application.html.slimをレイアウトファイルとして使う
-  layout "application"
-
   def index
     #️ ここにtomorrowとthe day afterどっちをクリックしたかをわけるif文を入れよ
 

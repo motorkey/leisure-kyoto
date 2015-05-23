9d8eadb9d59828955233bdd4bf3dd916f4c07170
motorkey <auagcumitsumo@gmail.com>
2015-05-23 22:58:20 +0900

    Update projects controller/view and fix layout application.html.slim

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,12 @@
 class ProjectsController < ApplicationController
+
+  # app/views/layout/application.html.slimをレイアウトファイルとして使う
+  layout "application"
+
+  def index
+    #️ ここにtomorrowとthe day afterどっちをクリックしたかをわけるif文を入れよ
+
+    @projects = Project.where(date: 0)
+  end
+
 end

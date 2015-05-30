f9c95e8df44fa923b9fe77d09b6954b007b9efea
motorkey <auagcumitsumo@gmail.com>
2015-05-30 23:42:49 +0900

    Add Event#create action. (problem: no redirect page from 'Create' button.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,5 +10,21 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+  def create
+    @event = Event.new(event_params)
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.json { render action: 'show', status: created, location: @event }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  private
+    def event_params
+      params.require(:event).permit(:name, :image, :description)
+    end
 end
 

b5e7ffeb77b198725415778c05f8c28ad2437f24
motorkey <auagcumitsumo@gmail.com>
2015-06-27 02:05:04 +0900

    Arrange migration files for production with Heroku

--- db/migrate/20150523125350_add_pic_to_projects.rb
+++
@@ -1,5 +0,0 @@
-class AddPicToProjects < ActiveRecord::Migration
-  def change
-    add_column :projects, :pic, :string
-  end
-end
--- db/migrate/20150528143733_remove_avatar_from_events.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAvatarFromEvents < ActiveRecord::Migration
-  def change
-    remove_column :events, :avatar, :string
-  end
-end

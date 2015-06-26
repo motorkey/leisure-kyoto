9b3787e2bbe93105730bfdf138f32578baf70d9c
motorkey <auagcumitsumo@gmail.com>
2015-06-26 14:41:26 +0900

    Struggle

--- app/controllers/event_reservations_controller.rb
+++ app/controllers/event_reservations_controller.rb
@@ -1,11 +1,13 @@
 class EventReservationsController < ApplicationController
   def create
-    reservation = EventReservation.new(event_reservation_params)
-    if reservation.save
+    @reservation = EventReservation.new(event_reservation_params)
+    if @reservation.save
       flash[:success] = "Your booking has been confirmed!"
       redirect_to pages_path
     else
-      render template: 'events/show'
+      # render events/show/:id ができない！
+      # てか長い！何かが問題！
+      redirect_to event_path(params[:event_reservation][:event_id], day: EventDay.find(params[:event_reservation][:event_day_id]).event_on) 
     end
   end
 
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,5 +12,16 @@ class EventsController < ApplicationController
     # dayの情報がなくこのページに来たら、トップに飛ばすかエラー出すか、今日のイベントを適当に流すか
     @reservation = @event.days.find_by(event_on: params[:day]).reservations.new
   end
+
+  # event_reservationsのcreateじゃなくてeventsのcreateでEventReservationを扱ってもよい？
+  #def create
+    #reservation = EventReservation.new(event_reservation_params)
+    #if reservation.save
+      #flash[:success] = "Your booking has been confirmed!"
+      #redirect_to pages_path
+    #else
+      #render 'show'
+    #end
+  #end
 end
 

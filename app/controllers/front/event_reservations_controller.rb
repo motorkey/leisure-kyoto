632af41bf2b3f7243e132a5064b6f5b2173e6f5e
motorkey <auagcumitsumo@gmail.com>
2015-07-05 07:32:33 +0900

    Fix

--- app/controllers/front/event_reservations_controller.rb
+++ app/controllers/front/event_reservations_controller.rb
@@ -2,12 +2,8 @@ require 'webpay'
 
 class Front::EventReservationsController < FrontController
   def purchase
-    webpay = WebPay.new('test_secret_91UfRU12fali0FVeQx8Yh677')
-    webpay.charge.create(
-      card: params['webpay-token'],
-      amount: 10_000,
-      currency: 'jpy'
-    )
+
+    # transactionを使え！！
 
     # POSTでcreateにredirect_toできないためcreateアクションの処理をここに書く
     reservation = EventReservation.new(event_reservation_params)
@@ -20,6 +16,13 @@ class Front::EventReservationsController < FrontController
       redirect_to event_path(params[:event_reservation][:event_id], day: EventDay.find(params[:event_reservation][:event_day_id]).event_on) 
     end
 
+    # 決済
+    webpay = WebPay.new('test_secret_91UfRU12fali0FVeQx8Yh677')
+    webpay.charge.create(
+      card: params['webpay-token'],
+      amount: 10_000,
+      currency: 'jpy'
+    )
     rescue WebPay::ErrorResponse::CardError => e
     # エラーハンドリング。発生する例外の種類がいくつか用意されているので、内容に応じて処理を書く
     #

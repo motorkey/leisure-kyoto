b69168e15aac04c3d9570e8504f1c586b079bed6
motorkey <auagcumitsumo@gmail.com>
2015-07-07 22:57:47 +0900

    Refactor

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -23,11 +23,6 @@ class Admin::EventsController < AdminController
   end
   def edit
     @event = Event.find(params[:id])
-    #(0..2).each do |i|
-      #if !@event.photos[i]
-        #@event.photos.build
-      #end
-    #end
   end
   def update
     @event = Event.find(params[:id])
@@ -37,13 +32,16 @@ class Admin::EventsController < AdminController
       render 'edit'
     end
   end
-
+  def destroy
+    if Event.find(params[:id]).destroy
+      redirect_to admin_events_path
+    else
+      render 'edit'
+    end
+  end
   private
     def event_params
       params.require(:event).permit(:name, :description, :address, :capacity)
     end
-    #def photo_params
-      #params.require(:event_photo).permit(:image)
-    #end
 end
 

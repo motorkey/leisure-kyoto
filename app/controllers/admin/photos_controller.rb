0854226d98dbbf069067489a89ba0791620cae9c
motorkey <auagcumitsumo@gmail.com>
2015-07-13 19:15:04 +0900

    Refactor mainly gmaps api

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -1,10 +1,12 @@
 class Admin::PhotosController < AdminController
   def create
+    # 選択せずにupload押したときにvalidationせよ！
     photo = EventPhoto.new
     photo.image = params[:image]
     photo.event_id = params[:event_id]
-    photo.save
-    redirect_to edit_admin_event_path(photo.event_id)
+    if photo.save
+      redirect_to edit_admin_event_path(photo.event_id)
+    end
   end
   def destroy
     EventPhoto.destroy(params[:id])

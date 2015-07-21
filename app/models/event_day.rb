0f2829c9057a48628f37e86bcd75c7ce3160f066
motorkey <auagcumitsumo@gmail.com>
2015-07-21 23:28:51 +0900

    Add proper validations to each model

--- app/models/event_day.rb
+++ app/models/event_day.rb
@@ -1,4 +1,5 @@
 class EventDay < ActiveRecord::Base
   belongs_to :event
+  validates :event_id, presence: true
   has_many :reservations, class_name: 'EventReservation', dependent: :destroy
 end
--- app/models/event_reservation.rb
+++ app/models/event_reservation.rb
@@ -1,6 +1,8 @@
 class EventReservation < ActiveRecord::Base
   belongs_to :event_day
+  validates :event_day_id, presence: true
   has_one :event, through: :event_day
+  validates :event_id, presence: true
   before_save { self.mail = mail.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
